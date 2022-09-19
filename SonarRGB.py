a6726f60b2c000114e778a7429e0d5b13d6c14d0
vmanka25 <vmanka25@charlottesvilleschools.org>
2022-09-19 15:18:10 -0400

    changed kaz to var

--- SonarRGB.py
+++ SonarRGB.py
@@ -1,6 +1,8 @@
 
-# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
-# SPDX-License-Identifier: MIT
+# Vincent Manka
+# 9/19/22
+# Sonar RGB
+
 import digitalio
 import simpleio
 import time
@@ -10,10 +12,10 @@ import neopixel
 from board import *
 
 sonar = adafruit_hcsr04.HCSR04(trigger_pin=board.D3, echo_pin=board.D2)
-Kaz = neopixel.NeoPixel(board.NEOPIXEL, 1)#connecting the neopixel on the board to the code
-Kaz.brightness = 0.1
+Var = neopixel.NeoPixel(board.NEOPIXEL, 1)#connecting the neopixel on the board to the code
+Var.brightness = 0.1
 1  #setting the brightness of the light, from 0-1 brightness
-KazOutput = 0
+VarOutput = 0
 Red = 0
 Green = 0
 Blue = 0
@@ -26,16 +28,16 @@ while True:
         if cm < 5:
             Blue = 0
             Red = 255
-            Kaz.fill((Red, 0, 0))#setting the color with RGB values
+            Var.fill((Red, 0, 0))#setting the color with RGB values
         elif cm > 5 and cm < 10:
             Green = 0
             Red = simpleio.map_range(cm, 5.1, 10, 255, 0)
             Blue = simpleio.map_range(Red, 0, 255, 255, 0)
-            Kaz.fill((Red, Green, Blue))
+            Var.fill((Red, Green, Blue))
         else:
             Blue = simpleio.map_range(cm, 10.1, 20, 255, 0)
             Green = simpleio.map_range(Blue, 0, 255, 255, 0)
-            Kaz.fill((0, Green, Blue))#setting the color with RGB values
+            Var.fill((0, Green, Blue))#setting the color with RGB values
     except RuntimeError:
         print("Retrying!")
     time.sleep(0.01)
