71c8012e3e53ce0ea166eeeeedd88398cabef963
vmanka25 <71350243+vmanka25@users.noreply.github.com>
2022-09-29 19:34:05 -0400

    Create servo2.py

---
+++ servo2.py
@@ -0,0 +1,32 @@
+# SPDX-FileCopyrightText: 2018 Kattni Rembor for Adafruit Industries
+#
+# SPDX-License-Identifier: MIT
+
+"""CircuitPython Essentials Servo standard servo example"""
+from digitalio import DigitalInOut, Direction, Pull
+import time
+import neopixel
+import board
+import pwmio
+from adafruit_motor import servo
+btn = DigitalInOut(board.D3)
+btn2 = DigitalInOut(board.D2)
+btn.direction = Direction.INPUT
+btn.pull = Pull.UP
+btn2.direction = Direction.INPUT
+btn2.pull = Pull.UP
+# create a PWMOut object on Pin A2.
+pwm = pwmio.PWMOut(board.D3, duty_cycle=2 ** 15, frequency=50)
+# Create a servo object, my_servo.
+my_servo = servo.Servo(pwm)
+
+while True:
+    time.sleep(.1)
+    if btn == True:
+        for angle in range(0, 180, 90):  # 0 - 180 degrees, 100 degrees at a time.
+            my_servo.angle = angle
+    elif btn2 == True:
+        for angle in range(180, 0, -90): # 180 - 0 degrees, 100 degrees at a time.
+            my_servo.angle = angle
+    else:
+        print("click a button man")
